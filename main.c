d7b6b07d3fa7de9dbaf4cd98fe036265da4ee7b8
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-11-25 23:15:22 +0100

    Update main.c
    Modifications :
    - Entrée du fichier en argument de ligne de commande

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include "traitementPGM.h"
 
-int main() {
-    int nbc, nbl;    
-    lire_fichier("test.pgm", &nbc, &nbl);
+int main(int argc, char *argv[]) {
+    int nbc, nbl, nbg;    
+    if (argc != 2) {
+        fprintf(stderr, "Entrez un fichier en argument\n");
+        return 1;
+    } else {
+        lire_fichier(argv[1], &nbc, &nbl, &nbg);
+        getDimensions(nbc, nbl);
+        getGrayLevel(nbg);
+    }  
     return 0;
 } 
