c4531f88539b91d95605b91614f1626370b6a8b2
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-11-28 20:23:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 
 int main(int argc, char *argv[]) {
     int nbc, nbl, nbg;    
+    unsigned char **image = NULL;
+
     if (argc != 2) {
         fprintf(stderr, "Entrez un fichier en argument\n");
-        return 1;
-    } else {
-        lire_fichier(argv[1], &nbc, &nbl, &nbg);
-        getDimensions(nbc, nbl);
-        getGrayLevel(nbg);
-    }  
+        return -1;
+    } 
+    lire_fichier(argv[1], &nbc, &nbl, &nbg, &image);
+    getDimensions(nbc, nbl);
+    getGrayLevel(nbg);
+    liberer_image(image);
     return 0;
 } 
