3c75ab06a9d2739e8a7a424d7be10212a56f93b1
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-12-01 15:19:55 +0100

    Update quadtree.c

--- quadtree.c
+++ quadtree.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct noeud {
-    unsigned char m;
-    int u;
-    int e;
-} Noeud;
+typedef struct Noeud {
+    unsigned char valeur;
+    unsigned char e;    
+    bool u;            
+    struct Noeud *premier;        
+    struct Noeud *second;         
+    struct Noeud *troisieme;         
+    struct Noeud *quatrieme;         
+} Noeud, *Quadtree;
 
-typedef struct {
-    int niveaux;
-    int taille_totale;
-    Noeud* noeuds;
-} Quadtree;
+Noeud *alloue_noeud(unsigned char valeur, unsigned char erreur, bool uniforme) {
+    Noeud *noeud = malloc(sizeof(Noeud));
+    noeud->valeur = valeur;
+    noeud->e = erreur;
+    noeud->u = uniforme;
+    noeud->premier = NULL;
+    noeud->second = NULL;
+    noeud->troisieme = NULL;
+    noeud->quatrieme = NULL;
+    return noeud;
+}
