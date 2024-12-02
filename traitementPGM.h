abfdca3cc20f7773b24b38a63cc16e3249d03b36
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-12-02 19:14:17 +0100

    Update traitementPGM.h

--- traitementPGM.h
+++ traitementPGM.h
@@ -3,9 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned char** construit_pixmap(int nbc, int nbl);
-void liberer_image(unsigned char** pixmap);
-int lire_fichier(const char *filename, int *nbc, int *nbl, int *nbg, unsigned char ***pixmap);
+unsigned char* construit_pixmap(int nbc, int nbl);
+int lire_fichier(const char *filename, int *nbc, int *nbl, int *nbg, unsigned char **pixmap);
 void getDimensions(int nbc, int nbl);
 void getGrayLevel(int nbg);
 
