de811f300cf0b603e230ee4cc868e8a1b0611655
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-12-02 18:55:31 +0100

    Update main.c
    Adaptation du main au changement de pixmap

--- main.c
+++ main.c
@@ -4,15 +4,16 @@
 
 int main(int argc, char *argv[]) {
     int nbc, nbl, nbg;    
-    unsigned char **image = NULL;
+    unsigned char *pixmap = NULL;
 
     if (argc != 2) {
         fprintf(stderr, "Entrez un fichier en argument\n");
         return -1;
     } 
-    lire_fichier(argv[1], &nbc, &nbl, &nbg, &image);
+    lire_fichier(argv[1], &nbc, &nbl, &nbg, &pixmap);
     getDimensions(nbc, nbl);
     getGrayLevel(nbg);
-    liberer_image(image);
+    
+    free(pixmap);
     return 0;
 } 
