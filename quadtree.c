017662d914eb4b354588a1e9bae63f4fc35a5921
RaedChami <90640921+RaedChami@users.noreply.github.com>
2024-12-06 00:20:13 +0100

    Update quadtree.c

--- quadtree.c
+++ quadtree.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct Noeud {
-    unsigned char valeur;
-    unsigned char e;    // Valeur d'erreur
-    bool u;             // Bit d'uniformité       
-    struct Noeud *premier;        
-    struct Noeud *second;         
-    struct Noeud *troisieme;         
-    struct Noeud *quatrieme;         
-} Noeud, *Quadtree;
+#include <stdbool.h>
+#include "quadtree.h"
 
 Noeud *alloue_noeud(unsigned char valeur, unsigned char erreur, bool uniforme) {
     Noeud *noeud = (Noeud*)malloc(sizeof(Noeud));
@@ -24,12 +16,34 @@ Noeud *alloue_noeud(unsigned char valeur, unsigned char erreur, bool uniforme) {
     return noeud;
 }
 
-unsigned char calcule_erreur(unsigned char *valeurs) {
-    /**
-     * @brief Calcule la valeur d'erreur du noeud.
-     * @param valeurs Valeurs des noeuds
-     * @return Renvoie valeur d'erreur du noeud
-     */
-    int somme = valeurs[0] + valeurs[1] + valeurs[2] + valeurs[3];
-    return somme % 4;
+Noeud *construire_quadtree(unsigned char *pixels, int largeur, int hauteur, int x, int y, int taille) {
+    if (taille == 1) {     
+        // Ajout de noeud au cas de base
+        return alloue_noeud(pixels[x + y * largeur], 0, true);      
+    }
+
+    Noeud *premier = construire_quadtree(pixels, largeur, hauteur, x, y, taille / 2);
+    Noeud *second = construire_quadtree(pixels, largeur, hauteur, x + taille / 2, y, taille / 2);
+    Noeud *troisieme = construire_quadtree(pixels, largeur, hauteur, x, y + taille / 2, taille / 2);
+    Noeud *quatrieme = construire_quadtree(pixels, largeur, hauteur, x + taille / 2, y + taille / 2, taille / 2);
+
+    bool uniforme = false;
+
+    unsigned char valeurs[4] = {premier->valeur, second->valeur, troisieme->valeur, quatrieme->valeur};
+    unsigned char moyenne = (valeurs[0] + valeurs[1] + valeurs[2] + valeurs[3]) / 4;
+    unsigned char erreur = (valeurs[0] + valeurs[1] + valeurs[2] + valeurs[3]) % 4;
+    if (!erreur) {
+        uniforme = ((premier->u && second->u && troisieme->u && quatrieme->u) && (premier->valeur == second->valeur && premier->valeur == troisieme->valeur && premier->valeur == quatrieme->valeur));  
+        // Codage du bit d'uniformité lorsque e = 0
+    }
+
+    Noeud *noeud_parent = alloue_noeud(moyenne, erreur, uniforme);
+    if (noeud_parent) {
+        noeud_parent->premier = premier;
+        noeud_parent->second = second;
+        noeud_parent->troisieme = troisieme;
+        noeud_parent->quatrieme = quatrieme;
+    }
+
+    return noeud_parent;
 }
