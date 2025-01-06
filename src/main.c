982161b3d0e2075a3007c218efca038360b3ed23
Raëd <90640921+RaedChami@users.noreply.github.com>
2025-01-06 15:15:45 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,7 +14,9 @@ int main(int argc, char *argv[]) {
     lire_fichier(argv[1], &nbc, &nbl, &nbg, &image);
     getDimensions(nbc, nbl);
     getGrayLevel(nbg);
+    afficher_premiers_pixels(image, nbc, nbl);  // Affiche les premiers pixels pour vérification *****DEBUG******
     Noeud *quadtree = construire_quadtree(image, nbc, nbl, 0, 0, nbc);
+    afficher_quadtree(quadtree, 0);
     free(image);
 
     return 0;
