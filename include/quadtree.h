e46a56f6433cafc5bbd92a95fb2b19f812d97704
Raëd <90640921+RaedChami@users.noreply.github.com>
2025-01-09 12:25:03 +0100

    Réadaptation du quadtree

--- include/quadtree.h
+++ include/quadtree.h
@@ -5,17 +5,14 @@
 #include <stdbool.h>
 
 typedef struct Noeud {
-    unsigned char valeur;
+    unsigned char valeur;  
     unsigned char e;    // Valeur d'erreur
     bool u;             // Bit d'uniformité       
-    struct Noeud *premier;        
-    struct Noeud *second;         
-    struct Noeud *troisieme;         
-    struct Noeud *quatrieme;         
+    struct Noeud *fils[4];  // fils          
 } Noeud, *Quadtree;
 
 Noeud *alloue_noeud(unsigned char valeur, unsigned char erreur, bool uniforme);
 Noeud *construire_quadtree(unsigned char *pixels, int largeur, int hauteur, int x, int y, int taille);
 void afficher_quadtree(Noeud *noeud, int niveau);
 
-#endif 
+#endif
