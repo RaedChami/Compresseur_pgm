7aca7462d6f90d2ffca512c2a207d2d557e11705
Raëd <90640921+RaedChami@users.noreply.github.com>
2025-01-09 15:27:20 +0100

    Traduction en anglais du fichier header du traitement de fichiers .PGM

--- include/traitementPGM.h
+++ include/traitementPGM.h
@@ -1,11 +1,9 @@
-#ifndef TRAITEMENTPGM_H
-#define TRAITEMENTPGM_H
+#ifndef TREAT_PGM_FILE_H
+#define TREAT_PGM_FILE_H
 #include <stdio.h>
 #include <stdlib.h>
 
-int lire_fichier(const char *filename, int *nbc, int *nbl, int *nbg, unsigned char **pixmap);
-void getDimensions(int nbc, int nbl);
-void getGrayLevel(int nbg);
+int read_pgm_file(const char *filename, int *nbc, int *nbl, int *nbg, unsigned char **pixmap);
 void afficher_premiers_pixels(unsigned char *pixmap, int nbc, int nbl);
 
 #endif
