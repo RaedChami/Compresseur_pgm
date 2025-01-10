3a2a43fdd3e9ef4c3f26fda6bcd096a73022c512
Raëd <90640921+RaedChami@users.noreply.github.com>
2025-01-10 15:48:38 +0100

    Update bit_writer.h

--- include/bit_writer.h
+++ include/bit_writer.h
@@ -9,7 +9,8 @@ typedef struct {
 
 BitStream *init_bit_stream(FILE *f);
 void pushbits(BitStream *curr, unsigned char data, int bit_count);
-void flush_bit_stream(BitStream *curr);
+unsigned char pullbits(BitStream *curr, int nb_bits);
+void finalize_bit_stream(BitStream *curr);
 void close_bit_stream(BitStream *curr);
 
 #endif
